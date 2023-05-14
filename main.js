a9e0ae9aa4297b0632784012101c4b8771a10125
Lucas Barbosa <lucasbarbosaemail@gmail.com>
2023-05-14 11:07:49 +0100

    Menus created

--- main.js
+++ main.js
@@ -3,8 +3,60 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const Menu = electron.Menu;
+
+
 
 let mainWindow = null;
+
+const menuItems = [
+    {
+        label: "Menu",
+        submenu: [
+            {
+                label: "About moveApp"
+            }
+        ]
+    },
+
+    {
+        label: "File",
+        submenu: [
+           
+            {
+                label: "Learn more"
+            },
+            {
+                label: "Help"
+            },
+            {
+                type: "separator",
+            },
+            {
+                label: "Exit"
+            }
+        ]
+    },
+
+    {
+        label: "Account",
+        submenu: [
+            {
+                label: "User Account"
+            },
+            {
+                type: "separator",
+            },
+            {
+                label: "Log out"
+            }
+        ]
+    },
+
+];
+
+const menu = Menu.buildFromTemplate(menuItems);
+Menu.setApplicationMenu(menu);
 
 app.on('window-all-closed', () => {
     if (Process.platform !== 'darwin') app.quit();
